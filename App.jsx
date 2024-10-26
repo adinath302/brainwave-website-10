0caa0aa14016b32db3eb61738e5c13ffacc2eca0
Adinath Gaware <139313629+adinath302@users.noreply.github.com>
2024-10-26 22:56:00 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
+import Button from "./Component/Button.jsx";
+import "./index.css";
+import Header from "./Component/Header.jsx";
+import Hero from "./Component/Hero.jsx";
+import Benefits from "./Component/Benefits.jsx";
+import Colllaboration from "./Component/Colllaboration.jsx";
+import Services from "./Component/Services.jsx";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benefits />
+        <Colllaboration />
+        <Services />
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
